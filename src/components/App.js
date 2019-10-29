8bff0818dfbf823e70a4d91f1ec5374ea8e8b9ef
Kamran Ali <kgali21@gmail.com>
2019-10-29 15:20:55 -0700

    have stuff displaying but doesnt look pretty

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Character from './Character';
 
 export default function App() {
   return (
     <>
       <Header />
       <Footer />
+      <Character
+        img="https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728"
+        name="Rick Sanchez"
+        species="Human" />
     </>
   );
 }
